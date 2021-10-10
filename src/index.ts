88d12f9deeec9a958bdb59c2e37b171eee175e22
Sanyam Jain <sanyamjaino98@gmail.com>
2021-10-11 02:09:08 +0530

    Finished Sorting

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@ import { CharactersCollection } from "./CharactersCollection";
 import { SinglyLinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
 
+new NumbersCollection([3, 0, -5, 10]).bubbleSort();
+new CharactersCollection("sanyam").bubbleSort();
 
+const sll = new SinglyLinkedList();
+sll.push(3);
+sll.push(0);
+sll.push(-5);
+sll.push(10);
+
+sll.bubbleSort()
+sll.print();
